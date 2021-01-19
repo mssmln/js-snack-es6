51d54e676e17b3b9ffbb50945ff97e6cf2d47acd
Massimiliano <m.croci.ly@gmail.com>
2021-01-19 14:29:35 +0100

    init

---
+++ 1/js/script.js
@@ -0,0 +1,16 @@
+// Non usate jQuery.
+// 1) Utilizzando const e/o let. Inserire un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+
+
+// variante con let
+// let n = parseInt(prompt('key in a number'));
+// if (n % 2 != 0) {
+//   n++;
+//   console.log('stampo il numero successivo: ' + n);
+// } else {
+//   console.log('stampo il numero digitato: ' + n);
+// }
+
+
+// variante con const

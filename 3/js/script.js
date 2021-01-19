2ea13900faac2c8c913e47f96da25adf6eac40c2
Massimiliano <m.croci.ly@gmail.com>
2021-01-19 15:54:49 +0100

    finished off with const

--- 3/js/script.js
+++ 3/js/script.js
@@ -2,10 +2,39 @@
 
 
 // variante let
-let word1 = prompt('type a word down');
-let word2 = prompt('type another word down');
-let longer = 'longer';
-let theLongest = 'longest';
+// let word1 = prompt('type a word down');
+// let word2 = prompt('type another word down');
+// let longer = 'longer';
+// let theLongest = 'longest';
+//
+// if (word1.length == word2.length) {
+//   document.getElementById('word').innerHTML = `
+//     <h3 class="${longer}">
+//       ${word1}
+//       ${word2}
+//     </h3>
+//   `
+// } else if (word1.length > word2.length){
+//   document.getElementById('word').innerHTML = `
+//     <h3 class="${theLongest}">
+//       ${word1}
+//     </h3>
+//   `
+// } else {
+//   document.getElementById('word').innerHTML = `
+//     <h3 class="${theLongest}">
+//       ${word2}
+//     </h3>
+//   `
+// }
+
+
+
+// variante const
+const word1 = prompt('type a word down');
+const word2 = prompt('type another word down');
+const longer = 'longer';
+const theLongest = 'longest';
 
 if (word1.length == word2.length) {
   document.getElementById('word').innerHTML = `
